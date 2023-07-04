24dd97ee450d7d4178bdcf5427b1ab85ff2aa216
Ahmet MENEVSE <ahmetmenevse@Ahmets-MBP.gateway>
2023-07-04 14:10:37 +0800

    added meme image with style

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,36 @@
 import React from 'react'
-
+import data from '../memesData'
 const Meme = () => {
+
+const [memeImage, setMemeImage] = React.useState("")
+
+function getImage() {
+  const randomMeme = data.data.memes
+  const randomIndex = Math.floor(Math.random() * randomMeme.length)
+  const url = randomMeme[randomIndex].url
+  setMemeImage(url)
+  
+}
   return (
-    <div className='form'>
-      <input 
-        type='text' 
-        placeholder='top text' 
-        className='form-input'
-        />
+    <main>
 
-      <input 
-        type='text' 
-        placeholder='buttom text' 
-        className='form-input'
-        />
+      <div className='form'>
+        <input 
+          type='text' 
+          placeholder='top text' 
+          className='form-input'
+          />
 
-        <button className='button'>Get a new meme image  🖼</button>
+        <input 
+          type='text' 
+          placeholder='buttom text' 
+          className='form-input'
+          />
 
-    </div>
+          <button onClick={getImage} className='button'>Get a new meme image  🖼</button>
+      </div>
+        <img src={memeImage} alt='' className='meme-img' />
+    </main>
   )
 }
 
