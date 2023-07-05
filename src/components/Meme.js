853ab63c184369522090e1ab7c7484e929e933ea
Ahmet MENEVSE <ahmetmenevse@Ahmets-MacBook-Pro.local>
2023-07-05 11:24:04 +0800

    added state image with state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import data from '../memesData'
+import memesData from '../memesData'
 const Meme = () => {
 
-const [memeImage, setMemeImage] = React.useState("")
+// const [memeImage, setMemeImage] = React.useState("")
+
+const [meme, setMeme] = React.useState({
+  topText: "",
+  bottomText: "",
+  randomImage: "http://i.imgflip.com/1bij.jpg"
+})
+
+const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
 function getImage() {
-  const randomMeme = data.data.memes
+  const randomMeme = allMemeImages.data.memes
   const randomIndex = Math.floor(Math.random() * randomMeme.length)
   const url = randomMeme[randomIndex].url
-  setMemeImage(url)
-  
+
+  setMeme(prevMeme => ({
+    ...prevMeme,
+    randomImage: url
+  }))
 }
   return (
     <main>
@@ -29,7 +40,7 @@ function getImage() {
 
           <button onClick={getImage} className='button'>Get a new meme image  🖼</button>
       </div>
-        <img src={memeImage} alt='' className='meme-img' />
+        <img src={meme.randomImage} alt='' className='meme-img' />
     </main>
   )
 }
